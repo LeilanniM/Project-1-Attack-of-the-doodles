2bc253a0bedd86c1ae1fabbc9ffdbdd6d53515c2
lmakoske1 <leilanni.dev@gmail.com>
2022-07-14 21:04:05 -0700

    badguy Spawns, moves and dissapear

--- js/app.js
+++ js/app.js
@@ -1,1 +1,34 @@
-console.log(`I'm Linked`);
+// class Badguy {
+//   constructor(name) {
+//     this.name = name;
+//   }
+// }
+
+// const badguy1 = new Badguy(); //instantiating first badguy
+
+//add eventlistener for START button:
+
+const start = document.querySelector("#start");
+start.addEventListener("click", spawn);
+
+//section 1:
+const main = document.querySelector("main");
+const badguy = document.createElement("div"); //creating a div for badguy
+main.appendChild(badguy); //appending it to main
+badguy.setAttribute("id", "badguy");
+
+//section 2:
+function spawn() {
+  setTimeout(() => {
+    badguy.classList.toggle("badguySpawns"); //turnON badguy (appears)
+    setTimeout(() => {
+      badguy.classList.toggle("badguyMoves"); //turnON badguyMoves
+      setTimeout(() => {
+        badguy.removeAttribute("class"); //turnOFF badguy1 & badguyMoves
+      }, 3000);
+    }, 1000);
+  }, 500);
+}
+
+//what do I want?
+//I want section 1 and section 2 to happen for
